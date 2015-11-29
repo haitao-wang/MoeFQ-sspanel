4d8c6930aae032a7c13b14819dc6e42ee3829a92
kirainmoe <i@imim.pw>
2015-11-29 12:24:37 +0800

    first commit

--- alipay/alipay.config.php
+++ alipay/alipay.config.php
@@ -20,13 +20,13 @@
  
 //↓↓↓↓↓↓↓↓↓↓请在这里配置您的基本信息↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
 //合作身份者id，以2088开头的16位纯数字
-$alipay_config['partner']		= '2088911939494302';
+$alipay_config['partner']		= '自己改吧';
 
 //收款支付宝账号
 $alipay_config['seller_email']	= 'moe@kkk.ky';
 
 //安全检验码，以数字和字母组成的32位字符
-$alipay_config['key']			= 'qo9c6j3teq29fiwj62mrfb03a6txabva';
+$alipay_config['key']			= 'niconiconiconiconiconiconi';
 
 
 //↑↑↑↑↑↑↑↑↑↑请在这里配置您的基本信息↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑
